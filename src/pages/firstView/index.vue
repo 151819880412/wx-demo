35fe1370c402256b5ea5cc02e87a9a231aad61d2
zhaoxin <zhaoxin@tofflon.com>
2022-03-05 15:48:47 +0800

    init

---
+++ src/pages/firstView/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class='firstViewContainer'>
+    <swiper>
+      <swiper-item>
+        <img src="/static/images/user.png" alt="">  
+      </swiper-item> 
+      <swiper-item>
+        <img src="/static/images/user.png" alt="">  
+      </swiper-item> 
+      <swiper-item>
+        <img src="/static/images/user.png" alt="">  
+      </swiper-item> 
+      <swiper-item>
+        <img src="/static/images/user.png" alt="">  
+      </swiper-item> 
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  props: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+  components: {},
+  beforeDestroy() {}
+};
+</script>
+<style >
+.firstViewContainer swiper{
+  width: 100%;
+  height: 100%;
+}
+.firstViewContainer swiper img{
+  width: 100%;
+  height: 100%;
+}
+</style>
